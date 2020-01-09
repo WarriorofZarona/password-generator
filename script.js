4458cae289badff5bc39ea8d591384dee9505d85
Andrew Bautista <andrewbautista85@gmail.com>
2020-01-09 09:27:00 -0500

    added conditional for password length between 8 and 128

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function userCriteria() {
+
+    var minChar = 8;
+    var maxChar = 128;
+    var passwordLength = parseInt(prompt("Enter the length of your password between 8 and 128"));
+
+    if (passwordLength < minChar) {
+
+        alert("Password length must be more than 8 characters!")
+
+    }
+
+    else if (passwordLength > maxChar) {
+
+        alert("Password length must be less than 128 characters!")
+
+    }
+
+    else {
+
+
+
+    }
+}
